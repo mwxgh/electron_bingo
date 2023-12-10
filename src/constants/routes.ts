929446aa593d71b2969dbb9a1a088a08e0b9ddc8
mwxgh <mwx6996@gmail.com>
2023-12-10 13:17:10 +0700

    try class structure

--- src/constants/routes.ts
+++
@@ -1,6 +0,0 @@
-export const ROUTES = {
-  HOME: '/',
-  LIST: '/list',
-  TEST: '/test',
-  NOT_FOUND: '/not-found',
-} as const
--- src/pages/NotFound/index.tsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="h-full w-full flex items-center justify-center">
-      404 Not found
-    </div>
-  )
-}
-
-export default NotFound
