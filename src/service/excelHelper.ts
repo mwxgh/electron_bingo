82940ba295d56ab57e1f189f87aa0e758d3a541d
mwxgh <mwx6996@gmail.com>
2023-12-16 03:06:52 +0700

    customize xlsx

--- src/service/excelHelper.ts
+++ src/service/excelHelper.ts
@@ -11,6 +11,15 @@ export const exportToExcel = (
 
   const ws = XLSX.utils.json_to_sheet(data)
 
+  // customize excel file
+  const wscols = [{ wch: 6 }, { wch: 7 }, { wch: 10 }, { wch: 20 }]
+  ws['!cols'] = wscols
+  const merge = [
+    { s: { r: 1, c: 0 }, e: { r: 2, c: 0 } },
+    { s: { r: 3, c: 0 }, e: { r: 4, c: 0 } },
+  ]
+  ws['!merges'] = merge
+
   const wb = XLSX.utils.book_new()
   XLSX.utils.book_append_sheet(wb, ws, 'Sheet1')
 
