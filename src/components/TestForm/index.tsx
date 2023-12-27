74d68deb8d628acfcb9a2268bc46ee54ca5dfee2
Nguyen Khanh <nguyen.van.khanh-c@sun-asterisk.com>
2023-12-28 02:29:57 +0700

    complete test

--- src/components/TestForm/index.tsx
+++ src/components/TestForm/index.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { Modal, Form, Input, FormInstance, Select, InputNumber } from 'antd'
 import Button from '@/components/Button'
 import './style.css'
-import { Test, typeLabels } from '@/types/common/database'
+import { Test, TestDetail, typeLabels } from '@/types/common/database'
+import { colorCodePalette } from '@/constants/common'
+import EyeIcon from '@/assets/svgs/eye.svg'
+import EarIcon from '@/assets/svgs/ear.svg'
 
 interface Props {
   title: string
@@ -10,6 +13,72 @@ interface Props {
   setIsOpen: (state: boolean) => void
   form: FormInstance
   onSubmit: (data: Test) => void
+  isCreate: boolean
+}
+
+interface QuestionDetailProps {
+  detail: TestDetail
+  index: number
+  onChange: (value: Partial<TestDetail>) => void
+}
+
+const QuestionDetail: React.FC<QuestionDetailProps> = ({
+  detail,
+  index,
+  onChange,
+}) => {
+  return (
+    <div className="flex items-center mb-[15px]">
+      <p className="mr-[15px]">Câu {index + 1}:</p>
+      <div className="mr-[15px]">
+        {detail.type === 'sight' ? (
+          <EyeIcon width="30" height="30" />
+        ) : (
+          <EarIcon width="30" height="30" />
+        )}
+      </div>
+      <div className="w-[170px] mr-[10px]">
+        <Select
+          defaultValue={detail.key}
+          disabled={detail.key === 'Enter'}
+          onChange={(value) => {
+            onChange({
+              key: value,
+            })
+          }}
+        >
+          {[1, 2, 3, 4, 5].map((item) => (
+            <Select.Option value={item.toString()}>
+              {item.toString()}
+            </Select.Option>
+          ))}
+        </Select>
+      </div>
+      {detail.value !== 'sound' && (
+        <div className="w-[120px]">
+          <Select
+            defaultValue={detail.value}
+            onChange={(value) => {
+              onChange({
+                value,
+              })
+            }}
+          >
+            {colorCodePalette.map((item) => (
+              <Select.Option value={item.hex}>
+                <div
+                  className="w-[75px] h-[20px]"
+                  style={{
+                    background: item.hex,
+                  }}
+                ></div>
+              </Select.Option>
+            ))}
+          </Select>
+        </div>
+      )}
+    </div>
+  )
 }
 
 const TestForm: React.FC<Props> = ({
@@ -18,15 +87,16 @@ const TestForm: React.FC<Props> = ({
   setIsOpen,
   onSubmit,
   form,
+  isCreate,
 }) => {
+
+
   const onFinish = (data: Test) => {
     onSubmit(data)
-    form.resetFields()
     setIsOpen(false)
   }
 
   const handleClose = () => {
-    form.resetFields()
     setIsOpen(false)
   }
 
@@ -48,7 +118,7 @@ const TestForm: React.FC<Props> = ({
           form={form}
           onFinish={onFinish}
         >
-          <Form.Item<Test>
+          <Form.Item
             label={<span className="text-lg ">Tên đề kiểm tra</span>}
             name="name"
             rules={[{ required: true }]}
@@ -56,14 +126,13 @@ const TestForm: React.FC<Props> = ({
           >
             <Input />
           </Form.Item>
-
-          <Form.Item<Test>
+          <Form.Item
             label={<span className="text-lg ">Loại đề kiểm tra</span>}
             name="type"
             rules={[{ required: true }]}
             className="mb-[30px]"
           >
-            <Select>
+            <Select disabled={!isCreate}>
               <Select.Option value="hear">{typeLabels.hear}</Select.Option>
               <Select.Option value="sight">{typeLabels.sight}</Select.Option>
               <Select.Option value="synthetic">
@@ -71,25 +140,51 @@ const TestForm: React.FC<Props> = ({
               </Select.Option>
             </Select>
           </Form.Item>
-
-          <Form.Item<Test>
+          <Form.Item
             label={<span className="text-lg ">Số câu hỏi</span>}
             name="quantity"
             rules={[{ required: true }]}
             className="mb-[30px]"
           >
-            <InputNumber min={5} max={20} />
+            <InputNumber min={5} max={20} disabled={!isCreate} />
           </Form.Item>
 
-          <Form.Item className="flex justify-center">
-            <Button
-              color="info"
-              onClick={() => {
-                setIsOpen(true)
-              }}
+          {!isCreate && (
+            <Form.Item
+              label={<span className="text-lg ">Chi tiết câu hỏi</span>}
+              name="details"
+              rules={[{ required: true }]}
+              className="mb-[30px]"
             >
-              Xác nhận
-            </Button>
+              <div className="h-[180px] overflow-y-scroll px-[15px] pt-[15px] border-[1px] border-slate-200 rounded-lg">
+                {form
+                  .getFieldValue('details')
+                  .map((item: TestDetail, index: number) => {
+                    const handleOnChange = (value: Partial<TestDetail>) => {
+                      const newDetailData = [...form.getFieldValue('details')]
+                      newDetailData[index] = {
+                        ...item,
+                        ...value,
+                      }
+
+                      form.setFieldValue('details', newDetailData)
+                    }
+
+                    return (
+                      <QuestionDetail
+                        key={index}
+                        index={index}
+                        detail={item}
+                        onChange={handleOnChange}
+                      />
+                    )
+                  })}
+              </div>
+            </Form.Item>
+          )}
+
+          <Form.Item className="flex justify-center">
+            <Button color="info">Xác nhận</Button>
           </Form.Item>
         </Form>
       )}
